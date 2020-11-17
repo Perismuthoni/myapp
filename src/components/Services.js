485d3ee79f79a6ddaa8db243eb1a966101ba9f27
Peris Muthoni <muthonimuriuki22@gmail.com>
2020-11-17 12:09:33 +0300

    commit

--- src/components/Services.js
+++ src/components/Services.js
@@ -3,19 +3,47 @@ import "../static/Services.css";
 
 function Services() {
   return (
-    <>
+    <div>
       <div className="container">
         <div class="row">
-          <p><b><center>OUR PRODUCTS</center></b></p>
+          <p>
+            <b>
+              <center>OUR PRODUCTS</center>
+            </b>
+          </p>
+        </div>
+        <div class="row">
+          <div className="row1"></div>
+          <div class="column">
+            <div className="audio">
+              <p>this is column orange</p>
+            </div>
+          </div>
+          <div class="column">this is column video and audio </div>
+          </div>
         </div>
         <div class="row">
           <div class="column">
-            <p>this is column one</p>
+            <p>this is column school of music</p>
           </div>
-          <div class="column">this is column two </div>
+          <div class="column">this is column green </div>
         </div>
-      </div>
-    </>
+        <div class="row">
+          <div class="column">
+            <p>this is column red</p>
+          </div>
+          <div class="column">this is column event management </div>
+        </div>
+        <div class="row">
+          <div class="column">
+            <p>this is column advertisement</p>
+          </div>
+          <div class="column">this is column blue </div>
+        </div>
+        <div/>
+        <div/>
+        </div>
+  
   );
 }
 
